f00a3ad076d256b601a15da5ecef64bab974e3f8
Dharmendra Rawat <dharmendrarawat743@gmail.com>
2026-05-25 01:01:46 +0530

    Completed JSON and UserType tasks

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JsonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // First JSON file
+            string userFile = "user.json";
+
+            string userResponse =
+                File.ReadAllText(userFile);
+
+            List<User> users =
+                JsonConvert.DeserializeObject<List<User>>(userResponse);
+
+            Console.WriteLine("Users:");
+
+            foreach (User user in users)
+            {
+                Console.WriteLine($"Name: {user.Name}");
+                Console.WriteLine($"Age: {user.Age}");
+                Console.WriteLine($"City: {user.City}");
+                Console.WriteLine();
+            }
+
+
+            // Second JSON file
+            string typeFile = "user_types.json";
+
+            string typeResponse =
+                File.ReadAllText(typeFile);
+
+            List<UserType> userTypes =
+                JsonConvert.DeserializeObject<List<UserType>>(typeResponse);
+
+            Console.WriteLine("User Types:");
+
+            foreach (UserType type in userTypes)
+            {
+                Console.WriteLine($"Name: {type.Name}");
+                Console.WriteLine($"Access: {type.AccessLevel}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ UserType.cs
@@ -0,0 +1,9 @@
+﻿namespace JsonExample
+{
+    public class UserType
+    {
+        public string Name { get; set; }
+
+        public string AccessLevel { get; set; }
+    }
+}
